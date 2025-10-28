76477dc28e28a3db5877babc8dec826bee1c441e
Pedro Machado de Paula <pmdpaula@gmail.com>
2025-10-28 19:23:38 -0300

    feat: add company data structure for business information

---
+++ src/data/companyData.ts
@@ -0,0 +1,19 @@
+export const companyData = {
+  name: "Patricia Siqueira Bolos",
+  representativeName: "Patricia Edwiges Alves de Siqueira",
+  representativeRole: "confeiteira",
+  address: "Rua Desembargador Izidro, 126 702 B - Tijuca - RJ",
+  companyIdType: "CNPJ",
+  companyId: "CPF",
+  companyIdNumber: "29.384.426/0001-78",
+  representativeIdType: "CPF",
+  representativeIdNumber: "02900486726",
+  bankData: {
+    bankName: "Itaú",
+    bankNumber: "341",
+    agency: "5649",
+    account: "07401-9",
+    pixType: "telefone",
+    pix: "21981629992",
+  },
+} as const;
